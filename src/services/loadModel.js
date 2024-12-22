0d83dcef93515383bccb403bb32a799b365d01a7
farhanfahreezy <mohdfarhanfahrezy@gmail.com>
2024-12-22 21:00:51 +0700

    service account

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,18 @@
-const tf = require('@tensorflow/tfjs-node');
+const tf = require("@tensorflow/tfjs-node");
+
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+  try {
+    // Use the model URL from environment variables if provided
+    const modelUrl = process.env.MODEL_URL;
+
+    // Load the model
+    const model = await tf.loadGraphModel(modelUrl);
+
+    console.log("Model loaded successfully.");
+    return model;
+  } catch (error) {
+    console.error("Error loading model:", error);
+    throw new Error("Failed to load the model.");
+  }
 }
 module.exports = loadModel;
